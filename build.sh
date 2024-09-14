bce967374f39eb055228de88daf509edc42e3bea
Caleb Snow <snow.calebw@gmail.com>
2024-09-14 15:21:34 -0400

    Switch from bash script to CMake

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FLAGS="-Wall -Werror -Wextra -O3 -std=c++23 -fno-exceptions -fno-rtti -fno-omit-frame-pointer -Wno-unused-parameter"
-CC="clang++"
-
-SRC="main.cpp phonology.cpp metropolitan_french.cpp"
-EXE="generator"
-
-$CC $FLAGS $SRC -o $EXE
